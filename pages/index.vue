f93b5d504cbfb64b1c4bf85e5ab200d7c101d647
seml404 <4040591@gmail.com>
2022-06-05 12:33:30 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<!--<template>
+  <div><h1>hello</h1></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+-->
+
+
+
+<script setup> 
+/* eslint-disable no-unused-vars */
+import NavbarMenu from '../components/NavbarMenu.vue'
+</script>
+
+<template>
+<div>
+<NavbarMenu/> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
+
+<style lang="scss" scoped>
